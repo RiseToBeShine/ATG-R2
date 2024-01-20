5d9d5a523652568141203a9bea0c9fef2be76f95
RiseToBeShine <157208756+RiseToBeShine@users.noreply.github.com>
2024-01-20 16:56:51 +0530

    Update ResetPassword.jsx

--- client/src/Pages/ResetPassword.jsx
+++ client/src/Pages/ResetPassword.jsx
@@ -16,7 +16,7 @@ const ResetPassword = () => {
 
     const handleSendPassword = () => {
         setBtnState('Resetting')
-        axios.post(`http://localhost:8800/api/auth/reset-password/${id}/${token}`, {password: newPassword})
+        axios.post(`https://atg-r2-api.vercel.app/api/auth/reset-password/${id}/${token}`, {password: newPassword})
         .then(res => {
             if(res.data.Status === "Success") {
                 console.log('success')
