5691e0c12e4f4f0bf46ed9c9be8483ef14f5ff7d
RiseToBeShine <157208756+RiseToBeShine@users.noreply.github.com>
2024-01-23 15:25:48 +0530

    Update AllPosts.jsx

--- client/src/Pages/AllPosts.jsx
+++ client/src/Pages/AllPosts.jsx
@@ -11,7 +11,7 @@ const AllPosts = ({mobile}) => {
     const storage = getStorage()
     const {username, updateUser, currUserId} = useContext(UserContext)
     const [postId,setPostId] = useState('')
-    const BASE_URL = 'https://atg-r2-api.vercel.app/api/'
+    const BASE_URL = 'https://atr-r2-server.onrender.com/api/'
     const [posts, setPosts] = useState([])
     const [newPost, setNewPost] = useState({
         caption: '',
